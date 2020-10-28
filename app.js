d7d2c661ba81a92f0b2318f00a2bd1417388626f
Jeel Patel <jeel.patel1111@gmail.com>
2020-10-29 02:21:50 +1100

    refactored app

--- app.js
+++ app.js
@@ -2,24 +2,31 @@ const express = require("express");
 const env = require("dotenv").config();
 const app = express();
 const productRoute = require("./routes/product");
+const salesRecordRoute = require("./routes/sales");
 const loginRoute = require("./routes/login");
-const bodyparser = require("body-parser");
 const cors = require("cors");
 
+//parsing request to json
 app.use(express.json());
-// app.use(cors({ origin: true }));
+app.use(cors({ origin: true }));
 
-// app.use(function (req, res, next) {
-//   res.setHeader("Access-Control-Allow-Origin", "*");
-//   next();
-// });
+app.use(function (req, res, next) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  next();
+});
 
-app.use("/login", loginRoute);
-
+//home route
 app.get("/", (req, res) => {
   res.send("Home Page");
 });
 
+//routing the request to the router
 app.use("/products", productRoute);
+app.use("/salesRecord", salesRecordRoute)
 
-app.listen(process.env.PORT, () => console.log(`Server started on port ${process.env.PORT}`));
+
+
+//defining the port
+const PORT = process.env.PORT | 3001
+//listening on the port
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
